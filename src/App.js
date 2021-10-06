ad36658c8e897fbe3f2208c5444fdc3556339556
coder-tanvir <tanvir_khan@sfu.ca>
2021-10-06 10:46:09 -0700

    Changed some ui

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from "react";
 import alanBtn from "@alan-ai/alan-sdk-web";
 import NewsCards from "./components/Newscards/NewsCards";
+import useStyles from "./styles.js";
+import logo from "./ainews.png";
 
-function App() {
+const App = () => {
+  const classes = useStyles();
   const [articles, setarticles] = useState([]);
   const alankey =
     "75d47121f03cff0f8864709c87d41a7c2e956eca572e1d8b807a3e2338fdd0dc/stage";
@@ -18,11 +21,23 @@ function App() {
   });
 
   return (
-    <div>
-      <h1>Alan Ai news</h1>
+    <div style={{ backgroundColor: "white" }}>
+      <div className={classes.logoContainer}>
+        <img
+          src={logo}
+          style={{
+            height: "300px",
+            width: "300px",
+            borderRadius: "50%",
+            marginTop: "10px",
+            background: "#5E31A7",
+            border: "solid royalblue",
+          }}
+        />
+      </div>
       <NewsCards articles={articles} />
     </div>
   );
-}
+};
 
 export default App;
